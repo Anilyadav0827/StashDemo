2ab05dd8dfc736d525aee3de131d93cca9db326f
Anilyadav0827 <anilyadavmekala6@gmail.com>
2024-02-13 09:30:44 +0530

    StashTesting

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public String name;
+    public String fatherName;
+
+    public HelloWorld(String name, String fatherName) {
+        this.name = name;
+        this.fatherName = fatherName;
+    }
+}
