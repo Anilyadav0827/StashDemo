6268686dc77ee7e95d7e7c675a639fc26168ef11
Anilyadav0827 <anilyadavmekala6@gmail.com>
2024-02-13 10:42:09 +0530

    StashTesting added friend StashTesting OrderApplication development is done

---
+++ src/OrderApplication.java
@@ -0,0 +1,9 @@
+public class OrderApplication {
+    public String noOfOrderItems;
+    public String productDetails;
+
+    public OrderApplication(String noOfOrderItems, String productDetails) {
+        this.noOfOrderItems = noOfOrderItems;
+        this.productDetails = productDetails;
+    }
+}
